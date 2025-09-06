761e53b8257da019058e3c61a24221bc8c45aaff
helloyatendra-kaushik <helloyatendra@gmail.com>
2025-09-06 23:44:52 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
 ## edit below variables as per your requirements -
-REPO_NAME = "Books-Recommender-System-Using-Machine-Learning"
+REPO_NAME = "Books-Recommender-System-Using-Machine-Learning-By-Yatendra-Kaushik"
 AUTHOR_USER_NAME = "entbappy"
 SRC_REPO = "src"
 LIST_OF_REQUIREMENTS = ['streamlit', 'numpy']
